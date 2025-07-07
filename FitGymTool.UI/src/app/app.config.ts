b84fff40b04af9e8e888aa17d8fe38173bf9591c
Debanjan Paul <debanjanpaul10@gmail.com>
2025-07-07 21:32:34 +0530

    Updated UI with icons

--- FitGymTool.UI/src/app/app.config.ts
+++ FitGymTool.UI/src/app/app.config.ts
@@ -8,17 +8,12 @@ import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
-import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
     providePrimeNG({
       ripple: true,
       theme: {
