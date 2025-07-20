ccf8fc845c7d7da2002003bb72d40d8d354b88e5
Debanjan Paul <debanjanpaul10@gmail.com>
2025-07-20 21:47:48 +0530

    Updated environment variables

--- FitGymTool.UI/src/env/environment.ts
+++ FitGymTool.UI/src/env/environment.ts
@@ -1,17 +1,21 @@
 export const environment = {
   production: true,
-  apiBaseUrl: '',
+  apiBaseUrl:
+    'https://app-webapi-fitgym-tool-hegdfmgnb0gdgva7.centralindia-01.azurewebsites.net',
   msalConfig: {
     auth: {
-      clientId: '',
-      authority: '',
+      clientId: 'a60b28a6-c429-47e7-bdb4-19502849f14e',
+      authority: 'https://login.microsoftonline.com/consumers/',
     },
-    scopes: ['', ''],
+    scopes: ['Users.Read', 'Users.Write'],
   },
   apiConfig: {
-    scopes: ['', ''],
-    uri: '',
-    apiScope: [''],
+    scopes: [
+      'api://0c49dfb9-3afe-4f24-934f-340be62b8cbd/Users.Read',
+      'api://0c49dfb9-3afe-4f24-934f-340be62b8cbd/Users.Write',
+    ],
+    uri: 'https://graph.microsoft.com/v1.0/me',
+    apiScope: ['api://0c49dfb9-3afe-4f24-934f-340be62b8cbd/Users.Write'],
   },
   idleConfig: {
     idleTimeoutMinutes: 30,
